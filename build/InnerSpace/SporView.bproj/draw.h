fb93e5ca5679919a8f673e0d30a61a03e7d82610
Patrick Cardona <patrick@patrick-mac.home>
2025-10-02 17:14:37 +0200

    Build removing

--- build/InnerSpace/SporView.bproj/draw.h
+++
@@ -1,7 +0,0 @@
-
-void cSetColor( int color, int ins );
-void cSetSpor( int x, int y );
-void cSetLine( int x1, int y1, int x2, int y2 );
-void NextCls( int maxX, int maxY );
-
-
--- build/WebServices-0.9.0/config.make
+++
@@ -1,4 +0,0 @@
-GNUTLS_CFLAGS=-I/usr/include/p11-kit-1 
-GNUTLS_LIBS=-lgnutls 
-NETTLE_CFLAGS=
-NETTLE_LIBS=-lnettle 
